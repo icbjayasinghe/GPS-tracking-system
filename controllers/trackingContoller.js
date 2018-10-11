f516e06c0990c685c5feb2de84bef1e1c06d8063
Thilakshika Udyani <Thilakshika@nikiniautomation.com>
2018-10-11 08:39:11 +0530

    add params to split function

--- controllers/trackingContoller.js
+++ controllers/trackingContoller.js
@@ -3,7 +3,7 @@ var hexToDec = require('hex-to-dec');
 
 var rawData = {   
     splitData : function (req, res, next) {
-        var testData = "080400000113fc208dff000f14f650209cca80006f00d60400040004030101150316030001460000015d0000000113fc17610b000f14ffe0209cc580006e00c0050001000403010115 0316010001460000015e0000000113fc284945000f150f00209cd200009501080400000004030101150016030001460000015d0000000113fc267c5b000f150a50209cccc0009300680400000004030101150016030001460000015b0004";    
+        var testData = req;
         let rsp = {};
         console.log("test:  " + testData);
         const tasks = [
@@ -12,15 +12,12 @@ var rawData = {
                 var date = new Date(dateDec);
                 rsp.date = date;
                 return cb(null, date);
-                //return date;
             },
             function longitude(cb) {
                 longi = hexToDec(testData.substring(22,30));
                 var longitude = longi/10000000;
                 rsp.longitude=longitude;
                 return cb(null, longitude);
-
-                //return longitude;
             },
             function latitude(cb) {
                 lati = hexToDec(testData.substring(30,38));
@@ -56,8 +53,6 @@ var rawData = {
             if (err) {
                 return next(err);
             }
-            //return res.json(results);
-           
         })
         console.log(rsp);
     }
