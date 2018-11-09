45ab696b3005fc35aae611a8eb6a6bea49b48d2a
Thilakshika Udyani <Thilakshika@nikiniautomation.com>
2018-11-09 11:34:28 +0530

    add history move to common facade

--- controllers/commonFacade.js
+++ controllers/commonFacade.js
@@ -3,5 +3,33 @@ var History = require('../models/history');
 var Vehicle = require('../models/vehicle');
 
 module.exports = {
+    create : function(req,res) {
+        Vehicle.viewVehicles(function(err,res){
+            res.forEach(element => {
+                var date = new Date();
+                var history = new History({
+                    date : date,
+                    userId : element.userId,
+                    vehicleNumber : element.vehicleNumber,
+                    trackingData : element.trackingData
+                });
+                Vehicle.removeAllTrackingData(element._id,function(err,res){
+                    if(err){
+                        console.log(err);
+                    }
+                    console.log(res);
+                })
+                History.newHistory(history,function(err,historyRes){
+                    if(err){
+                        console.log({success: false, msg: err});
+                    }
+                    console.log({success:true,history:historyRes});
+                })
+                console.log(element._id);
 
+                //CommonFacade.addTrackingDataToHistory(element._id);
+            });
+        })
+        res.json({success:true});
+    },
 }
--- controllers/historyController.js
+++ controllers/historyController.js
@@ -1,56 +1,11 @@
 var History  = require('../models/history');
 var CommonFacade = require('./commonFacade');
-var Vehicle = require('../models/vehicle');
 
 var history = {
     
     create : function(req,res) {
-        Vehicle.viewVehicles(function(err,res){
-            res.forEach(element => {
-                var date = new Date();
-
-                var history = new History({
-                    date : date,
-                    userId : element.userId,
-                    vehicleNumber : element.vehicleNumber,
-                    trackingData : element.trackingData
-                });
-                Vehicle.removeAllTrackingData(element._id,function(err,res){
-                    if(err){
-                        console.log(err);
-                    }
-                    console.log(res);
-                })
-                History.newHistory(history,function(err,historyRes){
-                    if(err){
-                        console.log({success: false, msg: err});
-                    }
-                    console.log({success:true,history:historyRes});
-                })
-                console.log(element._id);
-
-                //CommonFacade.addTrackingDataToHistory(element._id);
-            });
-
-        })
-        res.json({success:true});
-
-//     create : function(req,res, next) {
-//         Vehicle.find().then((vehicles) => {
-//             var resu;
-//             vehicles.forEach((vehicle) => {
-//                 var vId = vehicle._id;
-//                 console.log(vId);
-//                 resu = CommonFacade.addTrackingDataToHistory(vId, next);
-//             });
-//             res.json(resu);
-//         })
-
+        CommonFacade.create(req, res);
     },
-
-    // create : function(req,res, next) {
-    //     CommonFacade.addTrackingDataToHistory(req, res, next);
-    // },
 
     getHistory: function(req,res){
         History.getAll(function(err, history){
