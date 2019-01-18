1e57c08165a9b723ea7b6804666a64df76d34880
icbjayasinghe <icbjayasinghe@gmail.com>
2019-01-18 11:30:28 +0530

    change config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // 'secret': 'devdacticIsAwesome',
-    // 'database': 'mongodb://localhost/gps_tracking_system_db'
     'secret': 'devdacticIsAwesome',
-    'database': 'mongodb://Thilakshika:Thilakshika!123@ds141813.mlab.com:41813/gps_tracking_system_db'
+    'database': 'mongodb://localhost/gps_tracking_system_db'
   };
