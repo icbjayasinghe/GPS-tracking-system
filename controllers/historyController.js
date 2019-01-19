4c06947e58a21947f7731aef3616035d4ebf8699
Thilakshika Udyani <Thilakshika@nikiniautomation.com>
2019-01-19 10:39:58 +0530

    speeding details bug fixed

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -233,8 +233,9 @@ var history = {
                         }
                     }
                     var numberOfOverSpeedData = overSpeedIndexArray.length;
-                    if (numberOfOverSpeedData>2){
+                    if (numberOfOverSpeedData>0){
                         speedUpIndex = overSpeedIndexArray[numberOfOverSpeedData-1] ;
+                        console.log(numberOfOverSpeedData-1)
                         speedUpTime = trackingData[speedUpIndex].date;
 
                         for(j=(numberOfOverSpeedData-1);j>0;j--){
